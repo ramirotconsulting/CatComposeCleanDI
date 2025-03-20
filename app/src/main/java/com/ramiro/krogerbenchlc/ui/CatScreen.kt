bb1cb40bd95df056a1dbaabc7350290474fb7c54
Ramiro Castrejon <ramirocastrejon@MBPM2-LT4640J4YP-CASTREJON.local>
2025-03-20 13:54:53 -0400

    Image scale

--- app/src/main/java/com/ramiro/krogerbenchlc/ui/CatScreen.kt
+++ app/src/main/java/com/ramiro/krogerbenchlc/ui/CatScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
@@ -40,12 +41,18 @@ fun CatScreen(viewModel: CatsViewModel, modifier: Modifier){
             LazyColumn(modifier.fillMaxSize()) {
                 items(listResults){ cat ->
                     Card(modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)) {
-                        AsyncImage(model = cat.url, contentDescription = "Cat image")
+                        AsyncImage(model = cat.url, contentDescription = "Cat image", contentScale = ContentScale.FillWidth)
                         Text(text = "Cat ID: ${cat.id!!}")
                         Text(text = "Breed ID: ${cat.breeds?.get(0)?.id}")
-                        Text(text = "Name: ${cat.breeds?.get(0)?.name}")
-                        Text(text = "Description: ${cat.breeds?.get(0)?.description}")
-                        Text(text = "Lifespan: ${cat.breeds?.get(0)?.lifeSpan}")
+                        Text(text = "Name: ${cat.breeds?.joinToString{
+                            it.name ?: ""
+                            }}")
+                        Text(text = "Description: ${cat.breeds?.joinToString{
+                            it.description ?: ""
+                        }}")
+                        Text(text = "Lifespan: ${cat.breeds?.joinToString{
+                            it.lifeSpan ?: ""
+                        }}")
 
                     }
                 }
